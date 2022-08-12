61d8dcbb1564d3586e34fd83c32ba501159b344f
Leandro Alexis Borquez <110344935+LeanBorquez@users.noreply.github.com>
2022-08-12 01:04:38 +0100

    NFT

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x1E5D3957a7211b8A5eF3aa0121B63e6ed84B78F9";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
